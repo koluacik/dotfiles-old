04afd536d86f4b51a7d5a7176fb11207b060700a
Deniz Koluacik <koluacik@disroot.org>
2019-09-07 18:23:54 +0300

    new folder

---
+++ scripts/getshot.sh
@@ -0,0 +1,2 @@
+#eval $(cat ~/.scripts/.screenshot_command.txt)
+eval $(cat ~/dotfiles/scripts/screenshot_command.txt)
---
+++ scripts/script_launcher.sh
@@ -0,0 +1,1 @@
+exec $(find ~/dotfiles/scripts/ -type f -name "*.sh" -not -name "script_launcher.sh" | fzf)
